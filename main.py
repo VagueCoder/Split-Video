3bbc6978ba2251c07de13a7e288c22d80233e79e
VagueCoder <vaguecoder0to.n@gmail.com>
2021-06-06 19:59:28 +0530

    Basic Working Model

--- main.py
+++ main.py
@@ -1,34 +1,11 @@
-# TODO: Implement the commented snippet in actual code below.
-# import os
-# import sys
-# import argparse
-# from moviepy.editor import VideoFileClip
-
-# parser = argparse.ArgumentParser('Trimming/Splitting Operations on Video Files')
-# parser.add_argument('--file', nargs=1)
-
-# filename = parser.parse_args().file[0]
-
-# if not os.path.exists(filename):
-#     print(f"File Not Found Error: File '{filename}' not found in local.")
-#     sys.exit(0)
-# clip = VideoFileClip(filename)
-
-# print(clip.duration)
-
 
 import re
+import os
+import sys
+from pathlib import Path
 import argparse
-
-def len_from_size(total:int, size:float) -> float:
-    # TODO: Get the fraction and return start-end pairs rather than float value.
-    return 0
-
-units = {
-    "KB": 1024,
-    "MB": 1024**2,
-    "GB": 1024**3
-}
+from moviepy.editor import VideoFileClip
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
 def len_regex_type(arg_value, pat=re.compile(r"^\d+:?\d*$")):
     if not pat.match(arg_value):
@@ -39,30 +16,62 @@ def size_regex_type(arg_value, pat=re.compile(r"^\d+.?\d{0,2}[KMG]?B$")):
     if not pat.fullmatch(arg_value):
         raise argparse.ArgumentTypeError
     
-    total = 0
-    
     if arg_value[-2].isalpha():
         total = float(arg_value[:-2])*units[arg_value[-2:]]
     else:
         total = float(arg_value[:-1])
     return total
 
-parser = argparse.ArgumentParser('Trimming/Splitting Operations on Video Files')
+def ratio(an, ad, bn):
+    return (ad*bn)/an
 
-parser.add_argument('--file', nargs=1, metavar="FILENAME", type=str, required=True)
+def read_inputs():
+    units = {
+    "KB": 1024,
+    "MB": 1024**2,
+    "GB": 1024**3
+}
+    parser = argparse.ArgumentParser('Trimming/Splitting Operations on Video Files')
 
-group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument('--len', nargs=1,  metavar="LENGTH (mm[:ss])", type=len_regex_type)
-group.add_argument('--size', nargs=1,  metavar="SIZE (dd...d[.dd]B|KB|MB|GB)", type=size_regex_type)
+    parser.add_argument('--file', nargs=1, metavar="FILENAME", type=str, required=True)
 
-parser.print_help()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--len', nargs=1,  metavar="LENGTH - Format: mm[:ss]", type=len_regex_type)
+    group.add_argument('--size', nargs=1,  metavar="SIZE - Format: dd...d[.dd]B|KB|MB|GB)", type=size_regex_type)
 
-args = parser.parse_args()
-filename = args.file[0]
-length, size = 0.0, 0.0
-if args.len != None:
-    length = args.len[0]
-else:
-    size = args.size[0]
-    # TODO: As per len_from_size(), make changes here to receive the start-end pairs than just length of split.
-print(f"Filename: {filename}\nLength: {length}\nSize: {size}")
+    # parser.print_help()
+
+    args = parser.parse_args()
+    filename = args.file[0]
+    if not os.path.exists(filename):
+        print(f"File Not Found Error: File '{filename}' not found in local.")
+        sys.exit(0)
+    clip = VideoFileClip(filename)
+    
+    length, size = 0.0, 0.0
+    if args.len != None:
+        number, decimal_part = map(int, str(args.len[0]).split("."))
+        if decimal_part != 0:
+            length = round(ratio(60, decimal_part, 100)/10, 2)
+        length += number
+    else:
+        size = args.size[0]
+        filesize = Path(filename).stat().st_size
+        length = ratio(filesize, size, clip.duration)
+
+    return filename, length, clip.duration
+
+if __name__ == "__main__":
+    filename, split_len, duration = read_inputs()
+    splits = int(duration//split_len) +(1 if duration%split_len != 0 else 0)
+    print(f"Filename: {filename}\nLength: {split_len}\nDuration: {duration}\nNo. of Splits: {splits}")
+
+    name, ext = os.path.splitext(filename)
+    places = len(str(splits))
+    start = 0
+    for i in range(splits):
+        split_name = f"{name} - Chunk {str(i+1).zfill(places)}{ext}"
+        print(f"\nWriting chunk with name '{split_name}' from & to length [{start}, {duration if i == splits-1 else start+split_len}] of the actual file '{filename}' ...")
+        ffmpeg_extract_subclip(filename, start, start+split_len, targetname=split_name)
+        start += split_len
+        print(f"Written chunk '{split_name}' successfully!!")
